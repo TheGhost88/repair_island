c016a53ec9f082d7675a21a4ce864e0cf19e3516
brnielsen <47045697+brnielsen@users.noreply.github.com>
2020-01-31 22:56:55 -0500

    Adding Inventory Files

---
+++ Repair Island/Assets/Scripts/UI/Inventory/Inventory.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    [HideInInspector]
+    public EquipmentManager equipmentManager;
+
+    public delegate void OnItemChanged();
+    public OnItemChanged onItemChangedCallback;
+
+    public int space = 10;
+
+    public List<Item> items = new List<Item>();
+    public List<int> itemsInSlot = new List<int>();
+
+    public void Start()
+    {
+        //empty for now
+        //Could do some audio or other item setup here
+    }
+
+    public void Awake()
+    {
+        equipmentManager = GetComponent<EquipmentManager>();
+    }
+
+
+}
