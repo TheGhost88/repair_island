22ca4626e111008c0481129562fbd43103cea490
TheGhost88 <44216080+TheGhost88@users.noreply.github.com>
2020-02-01 18:54:21 -0500

    animation web in the works

--- Repair Island/Assets/Extra Sprite Editors/2d-extras-master/2d-extras-master/Tests/Editor/PlaceholderTests.cs
+++ Repair Island/Assets/Extra Sprite Editors/2d-extras-master/2d-extras-master/Tests/Editor/PlaceholderTests.cs
@@ -1,10 +1,10 @@
-using NUnit.Framework;
+//using NUnit.Framework;
 
-public class U2DExtrasPlaceholder 
-{
-    [Test]
-    public void PlaceHolderTest()
-    {
-        Assert.Pass("2D Extras tests are in a separate package.");
-    }
-}
+//public class U2DExtrasPlaceholder 
+//{
+  //  [Test]
+    //public void PlaceHolderTest()
+   //{
+     //   Assert.Pass("2D Extras tests are in a separate package.");
+   //}
+//}
