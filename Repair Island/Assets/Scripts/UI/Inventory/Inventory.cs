16bae213dc312448d91aae0e87a2b159cd744d23
brnielsen <47045697+brnielsen@users.noreply.github.com>
2020-02-01 08:10:20 -0500

    Setting up Inventory UI code

--- Repair Island/Assets/Scripts/UI/Inventory/Inventory.cs
+++ Repair Island/Assets/Scripts/UI/Inventory/Inventory.cs
@@ -12,6 +12,7 @@ public class Inventory : MonoBehaviour
     public OnItemChanged onItemChangedCallback;
 
     public int space = 10;
+    public int maxItemsInStack = 100;
 
     public List<Item> items = new List<Item>();
     public List<int> itemsInSlot = new List<int>();
@@ -29,33 +30,53 @@ public class Inventory : MonoBehaviour
 
     public bool Add(Item item)
     {
-        if(item != item.isDefaultItem)
+        if (item != item.isDefaultItem)
         {
-            if(items.Count >= space)
+            if (items.Count >= space)
             {
                 Debug.Log("Inventory Full");
                 return false;
             }
 
-            if(item.stackable == false)
+            if (item.stackable == false)
             {
                 items.Add(item);
                 itemsInSlot.Add(1);
             }
 
-            if(HasItem(item) == false && item.stackable == true)
+            if (HasItem(item) == false && item.stackable == true)
             {
                 items.Add(item);
                 itemsInSlot.Add(0);
             }
 
             item.equipmentManager = GetComponent<EquipmentManager>();
-            if(onItemChangedCallback != null)
+            if (onItemChangedCallback != null)
             {
                 onItemChangedCallback.Invoke();
             }
         }
         return true;
+    }
+
+    public void AddToStack(Item item, int amount)
+    {
+        int index = items.IndexOf(item);
+        Debug.Log(item.name + " is in slot " + index);
+
+        if ((itemsInSlot[index] + amount) >= maxItemsInStack)
+        {
+            itemsInSlot[index] = maxItemsInStack;
+        }
+    }
+
+    public void Remove(Item item)
+    {
+        items.Remove(item);
+        if (onItemChangedCallback != null)
+        {
+            onItemChangedCallback.Invoke();
+        }
     }
 
     private bool HasItem(Item item)
---
+++ Repair Island/Assets/Scripts/UI/Inventory/InventorySlot.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InventorySlot : MonoBehaviour
+{
+    public Image icon;
+    public Button removeButton;
+    public TextMeshProUGUI itemName;
+
+    [SerializeField]
+    private Button thisItemButton;
+
+    [SerializeField]
+    private TextMeshProUGUI numberOfItemsInSlotDisplay;
+
+    int numberOfIemsInStack = 0;
+
+    private Inventory inventory;
+
+    Item item;
+
+    private int index;
+
+    [SerializeField]
+    private InventoryControlPanel inventoryControlPanel;
+
+    private void Awake()
+    {
+        itemName.text = "Empty";
+        numberOfItemsInSlotDisplay.text = numberOfIemsInStack.ToString();
+        inventoryControlPanel = GetComponentInParent<InventoryControlPanel>();
+
+        int index = inventory.items.IndexOf(item);
+    }
+
+    private void Update()
+    {
+        if(item == null)
+        {
+            numberOfItemsInSlotDisplay.enabled = false;
+        }
+    }
+
+    public void AddItem(Item newItem)
+    {
+        item = newItem;
+        icon.sprite = item.icon;
+        icon.enabled = true;
+        removeButton.interactable = true;
+        itemName.text = item.name;
+    }
+
+    public void AddToNumberInSlot(Item newItem)
+    {
+        item = newItem;
+        int index = inventory.items.IndexOf(newItem);
+        numberOfIemsInStack = inventory.itemsInSlot[index];
+        numberOfItemsInSlotDisplay.enabled = true;
+
+        if(numberOfIemsInStack <= 1)
+        {
+            numberOfItemsInSlotDisplay.text = "";
+        }
+        else
+        {
+            numberOfItemsInSlotDisplay.text = numberOfIemsInStack.ToString();
+        }
+    }
+
+    public void ClearSlot()
+    {
+        item = null;
+        icon.sprite = null;
+        icon.enabled = false;
+        removeButton.interactable = false;
+        itemName.text = "Empty";
+        numberOfIemsInStack = 0;
+        numberOfItemsInSlotDisplay.text = numberOfIemsInStack.ToString();
+        numberOfItemsInSlotDisplay.enabled = false;
+    }
+
+    public void OnRemoveButton()
+    {
+        inventory.Remove(item);
+    }
+
+    public void UseItem()
+    {
+        if(item != null)
+        {
+            item.Use();
+        }
+    }
+}
