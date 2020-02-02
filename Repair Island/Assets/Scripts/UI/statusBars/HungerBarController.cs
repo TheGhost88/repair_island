9b520199662357aa898b2b749c8b225459ed4ed3
TheGhost88 <44216080+TheGhost88@users.noreply.github.com>
2020-02-01 23:03:26 -0500

    merge

---
+++ Repair Island/Assets/Scripts/UI/statusBars/HungerBarController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HungerBarController : MonoBehaviour
+{
+    private Slider hungerBar;
+    private int currentHGR = 100;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        hungerBar = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hungerBar.value = currentHGR;
+    }
+
+    public void changeHP(int dHGR)
+    {
+        currentHGR += dHGR;
+    }
+}
---
+++ Repair Island/Assets/Scripts/UI/statusBars/StaminaBarController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StaminaBarController : MonoBehaviour
+{
+    private Slider staminaBar;
+    private int currentSTM = 100;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        staminaBar = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        staminaBar.value = currentSTM;
+    }
+
+    public void changeHP(int dSTM)
+    {
+        currentSTM += dSTM;
+    }
+}
