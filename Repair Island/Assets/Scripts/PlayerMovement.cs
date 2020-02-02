6eecb95060092738dd12218d457803153814bdd2
TheGhost88 <44216080+TheGhost88@users.noreply.github.com>
2020-02-02 12:22:31 -0500

    swing working
    left mouse button

--- Repair Island/Assets/Scripts/PlayerMovement.cs
+++ Repair Island/Assets/Scripts/PlayerMovement.cs
@@ -12,6 +12,12 @@ public class PlayerMovement : MonoBehaviour
     int whichAxis = 0; //0 not moving. 1 LR. 2 UD
     public bool canRun = false;
 
+    public GameObject slashF, slashB, slashL, slashR;
+
+    public bool axeEquiped = false;
+
+    bool facingB, facingF, facingL, facingR = false;
+
     public Animator animator;
 
     // Update is called once per frame
@@ -22,7 +28,67 @@ public class PlayerMovement : MonoBehaviour
         animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
         animator.SetBool("canRun", canRun);
 
-        if(Input.GetKey(KeyCode.LeftShift))
+
+
+        if (Input.GetKey(KeyCode.Mouse0) && axeEquiped && (Input.GetAxis("Horizontal") > 0 || facingR == true)) //right swing 
+        {
+            facingB = false;
+            facingF = false;
+            facingL = false;
+            facingR = true;
+            slashB.GetComponent<SpriteRenderer>().enabled = false;
+            slashF.GetComponent<SpriteRenderer>().enabled = false;
+            slashL.GetComponent<SpriteRenderer>().enabled = false;
+            slashR.GetComponent<SpriteRenderer>().enabled = true;
+        }
+        else if (Input.GetKey(KeyCode.Mouse0) && axeEquiped && (Input.GetAxis("Horizontal") < 0 || facingL == true)) //left swing 
+        {
+            facingB = false;
+            facingF = false;
+            facingL = true;
+            facingR = false;
+            slashB.GetComponent<SpriteRenderer>().enabled = false;
+            slashF.GetComponent<SpriteRenderer>().enabled = false;
+            slashL.GetComponent<SpriteRenderer>().enabled = true;
+            slashR.GetComponent<SpriteRenderer>().enabled = false;
+        }
+        else if (Input.GetKey(KeyCode.Mouse0) && axeEquiped && (Input.GetAxis("Vertical") < 0 || facingF == true)) //Forward swing 
+        {
+            facingB = false;
+            facingF = true;
+            facingL = false;
+            facingR = false;
+            slashB.GetComponent<SpriteRenderer>().enabled = false;
+            slashF.GetComponent<SpriteRenderer>().enabled = true;
+            slashL.GetComponent<SpriteRenderer>().enabled = false;
+            slashR.GetComponent<SpriteRenderer>().enabled = false;
+        }
+        else if (Input.GetKey(KeyCode.Mouse0) && axeEquiped && (Input.GetAxis("Vertical") > 0 || facingB == true)) //back swing 
+        {
+            facingB = true;
+            facingF = false;
+            facingL = false;
+            facingR = false;
+            slashB.GetComponent<SpriteRenderer>().enabled = true;
+            slashF.GetComponent<SpriteRenderer>().enabled = false;
+            slashL.GetComponent<SpriteRenderer>().enabled = false;
+            slashR.GetComponent<SpriteRenderer>().enabled = false;
+        }
+        else
+        {
+            facingB = false;
+            facingF = false;
+            facingL = false;
+            facingR = false;
+            slashB.GetComponent<SpriteRenderer>().enabled = false;
+            slashF.GetComponent<SpriteRenderer>().enabled = false;
+            slashL.GetComponent<SpriteRenderer>().enabled = false;
+            slashR.GetComponent<SpriteRenderer>().enabled = false;
+        }
+
+
+
+        if (Input.GetKey(KeyCode.LeftShift))
         {
             canRun = true;
             walkspeed = 6.0f;
@@ -33,14 +99,14 @@ public class PlayerMovement : MonoBehaviour
             walkspeed = 3.0f;
         }
 
-       
-            leftRightMove = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
-            transform.position = transform.position + leftRightMove * Time.deltaTime * walkspeed;
-            upDownMove = new Vector3(0, Input.GetAxis("Vertical"), 0);
-            transform.position = transform.position + upDownMove * Time.deltaTime * walkspeed;
-       
 
-        
+        leftRightMove = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        transform.position = transform.position + leftRightMove * Time.deltaTime * walkspeed;
+        upDownMove = new Vector3(0, Input.GetAxis("Vertical"), 0);
+        transform.position = transform.position + upDownMove * Time.deltaTime * walkspeed;
+
+
+
 
 
     }
